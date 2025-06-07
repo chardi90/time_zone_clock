ca4ea3a2e80d7adeef32fec2c3ec37414328cbc7
chardi90 <chardi90@hotmail.com>
2025-06-07 19:02:52 +0100

    Created basic HTML selector for cities & added JS alerts for each one

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
-function sayHello() {
-  let helloElement = document.querySelector("#hello");
-  helloElement.innerHTML = "Bonjour!";
+const citySelect = document.querySelector("#city-dropdown");
+
+function parisDateTime() {
+  let parisTime = moment()
+    .tz("Europe/Paris")
+    .format("dddd, MMMM D, YYYY h:mm A");
+  return `It is ${parisTime} in Paris, France.`;
 }
 
-setTimeout(sayHello, 3000);
+function tokyoDateTime() {
+  let tokyoTime = moment().tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:mm A");
+  return `It is ${tokyoTime} in Tokyo, Japan.`;
+}
+
+function sydneyDateTime() {
+  let sydneyTime = moment()
+    .tz("Australia/Sydney")
+    .format("dddd, MMMM D, YYYY h:mm A");
+  return `It is ${sydneyTime} in Sydney, Australia.`;
+}
+
+function alertSelectedCountry(event) {
+  if (event.target.value.length > 0) {
+    if (event.target.value === "paris") {
+      alert(parisDateTime());
+    }
+    if (event.target.value === "tokyo") {
+      alert(tokyoDateTime());
+    }
+    if (event.target.value === "sydney") {
+      alert(sydneyDateTime());
+    }
+  }
+}
+
+citySelect.addEventListener("change", alertSelectedCountry);
