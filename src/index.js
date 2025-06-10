c9b30cbba6d4bdfabfd493891c4b4ae5c0aa92ab
chardi90 <chardi90@hotmail.com>
2025-06-10 12:12:04 +0100

    Added JS function to replace display city once selected
    - Added extra options to dropdown select
    - Added id to cities displayed to target with JS
    - Deleted useless JS alert functions
    - Created function to display selected city
    - Added if statement to original display function to avoid errors

--- src/index.js
+++ src/index.js
@@ -1,76 +1,71 @@
-const citySelect = document.querySelector("#city-dropdown");
+const citySelectElement = document.querySelector("#city-dropdown");
 
-function parisDateTime() {
-  let parisTime = moment()
-    .tz("Europe/Paris")
-    .format("dddd, MMMM D, YYYY h:mm A");
-  return `It is ${parisTime} in Paris, France.`;
-}
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityDate = moment().tz(cityTimeZone).format("Do MMMM YYYY");
+  let cityTime = moment()
+    .tz(cityTimeZone)
+    .format("h:mm:ss [<small>]A[</small>]");
+  let citiesElement = document.querySelector("#cities-displayed");
 
-function tokyoDateTime() {
-  let tokyoTime = moment().tz("Asia/Tokyo").format("dddd, MMMM D, YYYY h:mm A");
-  return `It is ${tokyoTime} in Tokyo, Japan.`;
-}
-
-function sydneyDateTime() {
-  let sydneyTime = moment()
-    .tz("Australia/Sydney")
-    .format("dddd, MMMM D, YYYY h:mm A");
-  return `It is ${sydneyTime} in Sydney, Australia.`;
-}
-
-function alertSelectedCountry(event) {
-  if (event.target.value.length > 0) {
-    if (event.target.value === "paris") {
-      alert(parisDateTime());
-    }
-    if (event.target.value === "tokyo") {
-      alert(tokyoDateTime());
-    }
-    if (event.target.value === "sydney") {
-      alert(sydneyDateTime());
-    }
-  }
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityDate}</div>
+    </div>
+    <div class="time">${cityTime}</div>
+  </div>
+  `;
 }
 
 setInterval(function () {
   function displayCityOne() {
     let cityOneElement = document.querySelector("#city-one");
-    let cityOneDateElement = cityOneElement.querySelector(".date");
-    let cityOneTimeElement = cityOneElement.querySelector(".time");
-    let cityOneDate = moment().tz("America/Los_Angeles").format("Do MMMM YYYY");
-    let cityOneTime = moment()
-      .tz("America/Los_Angeles")
-      .format("h:mm:ss [<small>]A[</small>]");
+    if (cityOneElement) {
+      let cityOneDateElement = cityOneElement.querySelector(".date");
+      let cityOneTimeElement = cityOneElement.querySelector(".time");
+      let cityOneDate = moment()
+        .tz("America/Los_Angeles")
+        .format("Do MMMM YYYY");
+      let cityOneTime = moment()
+        .tz("America/Los_Angeles")
+        .format("h:mm:ss [<small>]A[</small>]");
 
-    cityOneDateElement.innerHTML = cityOneDate;
-    cityOneTimeElement.innerHTML = cityOneTime;
+      cityOneDateElement.innerHTML = cityOneDate;
+      cityOneTimeElement.innerHTML = cityOneTime;
+    }
   }
 
   function displayCityTwo() {
     let cityTwoElement = document.querySelector("#city-two");
-    let cityTwoDateElement = cityTwoElement.querySelector(".date");
-    let cityTwoTimeElement = cityTwoElement.querySelector(".time");
-    let cityTwoDate = moment().tz("Europe/Paris").format("Do MMMM YYYY");
-    let cityTwoTime = moment()
-      .tz("Europe/Paris")
-      .format("h:mm:ss [<small>]A[</small>]");
+    if (cityTwoElement) {
+      let cityTwoDateElement = cityTwoElement.querySelector(".date");
+      let cityTwoTimeElement = cityTwoElement.querySelector(".time");
+      let cityTwoDate = moment().tz("Europe/Paris").format("Do MMMM YYYY");
+      let cityTwoTime = moment()
+        .tz("Europe/Paris")
+        .format("h:mm:ss [<small>]A[</small>]");
 
-    cityTwoDateElement.innerHTML = cityTwoDate;
-    cityTwoTimeElement.innerHTML = cityTwoTime;
+      cityTwoDateElement.innerHTML = cityTwoDate;
+      cityTwoTimeElement.innerHTML = cityTwoTime;
+    }
   }
 
   function displayCityThree() {
     let cityThreeElement = document.querySelector("#city-three");
-    let cityThreeDateElement = cityThreeElement.querySelector(".date");
-    let cityThreeTimeElement = cityThreeElement.querySelector(".time");
-    let cityThreeDate = moment().tz("Asia/Tokyo").format("Do MMMM YYYY");
-    let cityThreeTime = moment()
-      .tz("Asia/Tokyo")
-      .format("h:mm:ss [<small>]A[</small>]");
+    if (cityThreeElement) {
+      let cityThreeDateElement = cityThreeElement.querySelector(".date");
+      let cityThreeTimeElement = cityThreeElement.querySelector(".time");
+      let cityThreeDate = moment().tz("Asia/Tokyo").format("Do MMMM YYYY");
+      let cityThreeTime = moment()
+        .tz("Asia/Tokyo")
+        .format("h:mm:ss [<small>]A[</small>]");
 
-    cityThreeDateElement.innerHTML = cityThreeDate;
-    cityThreeTimeElement.innerHTML = cityThreeTime;
+      cityThreeDateElement.innerHTML = cityThreeDate;
+      cityThreeTimeElement.innerHTML = cityThreeTime;
+    }
   }
 
   displayCityOne();
@@ -78,4 +73,4 @@ setInterval(function () {
   displayCityThree();
 }, 1000);
 
-citySelect.addEventListener("change", alertSelectedCountry);
+citySelectElement.addEventListener("change", updateCity);
